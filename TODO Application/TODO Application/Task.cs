b3a545d79f7fe5e99a82efcab87c7d016a7b336f
KianZhou <Q.Zhou7@newcastle.ac.uk>
2018-11-16 17:06:11 +0800

    todo app

---
+++ TODO Application/TODO Application/Task.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TODO_Application
+{
+    public class Task
+    {
+
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public bool IsComplete { get; set; }
+        public Task(string v)
+        {
+            this.Name = v;
+        }
+
+        internal void ShowState()
+        {
+            if (IsComplete)
+            {
+                Console.WriteLine($"[x] {Name}");
+            }
+            else
+            {
+                Console.WriteLine($"[ ] {Name}");
+            }
+            
+        }
+    }
+}
